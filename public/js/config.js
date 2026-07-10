d953754b24ed01db3fa92e7a3f07038542d6479c
Zougraf Dz <zougzed@gmail.com>
2026-07-10 21:00:37 +0200

    demo version

--- public/js/config.js
+++ public/js/config.js
@@ -1,6 +1,6 @@
 var CONFIG = {
-  API_URL: 'http://localhost:5000/api',
-  SOCKET_URL: 'http://localhost:5000',
+  API_URL: 'https://hivoraa.fly.dev/api',
+  SOCKET_URL: 'https://hivoraa.fly.dev',
   GOOGLE_CLIENT_ID: '626986280198-sti102t5u7fh6jv7fsfl9iqukn0l7rgf.apps.googleusercontent.com',
-  REDIRECT_URI: 'http://localhost:5000/authentication'
+  REDIRECT_URI: 'https://hivoraa.fly.dev/authentication'
 };
